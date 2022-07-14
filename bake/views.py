76bb2731632ad5a1ce2540010a9a9d3166cb21a7
Your Name <you@example.com>
2022-07-14 18:38:59 +0900

    something change

--- bake/views.py
+++ bake/views.py
@@ -6,6 +6,3 @@ def main(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'bake/main.html', {'posts': posts})
 
-
-def main(request):
-    return render(request, 'bake/main.html', {})
