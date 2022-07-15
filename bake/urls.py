56e37e124542fe58ba57573f878b4a2c710cc8c3
parks <jks@gmail.com>
2022-07-15 13:51:43 +0900

    change html urls views

--- bake/urls.py
+++ bake/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+import bake
 from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path("bake/pan/", bake.views.pan, name='pan'),
+    path("bake/ja/", bake.views.ja, name='ja'),
+    path("bake/yong/", bake.views.yong, name='yong'),
 ]
--- bake/views.py
+++ bake/views.py
@@ -5,4 +5,9 @@ from .models import Post
 def main(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'bake/main.html', {'posts': posts})
-
+def pan(request):
+    return render(request, 'bake/pan.html', {})
+def ja(request):
+    return render(request, 'bake/ja.html', {})
+def yong(request):
+    return render(request, 'bake/yong.html', {})
