88dda0cbfc86a19f8d7d66ef20a9924674431b9c
Your Name <you@example.com>
2022-07-18 16:49:10 +0900

    0718-5 change

--- bake/views.py
+++ bake/views.py
@@ -11,5 +11,5 @@ def ja(request):
     return render(request, 'bake/ja.html', {})
 def yong(request):
     return render(request, 'bake/yong.html', {})
-def new(request):
+def jawrite(request):
     return render(request, 'bake/jawrite.html', {})
